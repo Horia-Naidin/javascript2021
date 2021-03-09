2ab47996eeb50a439a3403328a55fb515cdc4536
Adrian Adiaconitei <adrian@hipo.ro>
2021-03-09 10:16:10 +0200

    curs 1

---
+++ curs1/jsex2.php
@@ -0,0 +1,14 @@
+<html>  
+<head>  
+</head>  
+<body>  
+<h3>Afisarea textului cu JavaScript</h3> <hr/>  
+<script type="text/javascript">  
+document.write("Acest mesaj este scris cu JavaScript");  
+</script> 
+<br>
+<?php echo "Acest mesaj este scris cu PHP";?> 
+<br>
+Acest mesaj este scris din HTML
+</body>  
+</html>
